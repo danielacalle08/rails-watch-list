456f4677f18f9c0caa9bba43c7f1c28b7d2a5ba7
Daniela Calle Meneses <danielacalle08@hotmail.com>
2022-09-23 17:48:24 +0200

    Fixing errors - delete movies

--- db/migrate/20220921173240_create_reviews.rb
+++ db/migrate/20220921173240_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.text :comment
-      t.decimal :rating
+      t.integer :rating
       t.references :list, null: false, foreign_key: true
 
       t.timestamps
